f323557a236fed1b4c40128b27437c815f374c3f
peerless2012 <peerless2012@126.com>
2026-03-31 21:49:35 +0800

    更新构建脚本，支持tiny模式，方便调试。

--- entry/hvigorfile.ts
+++ entry/hvigorfile.ts
@@ -1,8 +1,28 @@
-import { hapTasks } from '@ohos/hvigor-ohos-plugin';
+import { hapTasks, OhosHapContext, OhosPluginId, Target } from '@ohos/hvigor-ohos-plugin';
+import { hvigor, HvigorNode, HvigorPlugin } from '@ohos/hvigor';
 
+export function dynamicDependencyPlugin(): HvigorPlugin {
+    return {
+        pluginId: 'dynamicDependencyPlugin',
+        async apply(currentNode: HvigorNode): Promise<void> {
+            const hapContext = currentNode.getContext(OhosPluginId.OHOS_HAP_PLUGIN) as OhosHapContext;
+            hapContext?.targets((target: Target) => {
+                const targetName = target.getTargetName();
+                const dependency = hapContext.getDependenciesOpt();
+                if (targetName === 'system') {
+                    dependency['@ohpg/system'] = '@param:ohpg';
+                } else if (targetName === 'default') {
+                    dependency['@ohpg/mpv'] = '@param:ohpg';
+                }
+                hapContext.setDependenciesOpt(dependency)
+            });
+        }
+    };
+}
 export default {
-    /* Built-in plugin of Hvigor. It cannot be modified. */
-    system: hapTasks,
-    /* Custom plugin to extend the functionality of Hvigor. */
-    plugins:[]
+    system: hapTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
+    plugins:[
+        /* Custom plugin to extend the functionality of Hvigor. */
+        dynamicDependencyPlugin()
+    ]
 }
